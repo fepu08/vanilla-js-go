7468001dfd5255b9dcbe60c06b205a3c62d4f319
fepu08 <ferencbesenyei@protonmail.com>
2025-07-16 21:40:37 +0200

    setup postgres connection

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/fepu08/vanilla-js-go/handlers"
 	"github.com/fepu08/vanilla-js-go/logger"
+	"github.com/joho/godotenv"
 )
 
 func initializeLogger() *logger.Logger {
@@ -22,13 +25,30 @@ func initializeLogger() *logger.Logger {
 func main() {
 	logInstance := initializeLogger()
 
+	// Reading Environment Variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("No .env file was available")
+	}
+
+	// Connect to DB
+	dbConnStr := os.Getenv("DATABASE_URL")
+	if dbConnStr == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
+
+	db, err := sql.Open("postgres", dbConnStr)
+	if err != nil {
+		log.Fatal("Failed to connect to the DB: %v", err)
+	}
+	defer db.Close()
+
 	movieHandler := handlers.NewMovieHandler()
 
 	http.Handle("/", http.FileServer(http.Dir("public")))
 	http.HandleFunc("/api/movies/top", movieHandler.GetTopMovies)
 
 	const addr = ":8080"
-	err := http.ListenAndServe(addr, nil)
+	err = http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatalf("Server failed: %v", err)
 		logInstance.Error("Server failer %v", err)
