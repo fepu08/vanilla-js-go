04b2b6161ed319065f48d8220dde251975e22ca8
fepu08 <ferencbesenyei@protonmail.com>
2025-07-18 22:06:08 +0200

    movie handler

--- handlers/movie_handlers.go
+++ handlers/movie_handlers.go
@@ -41,27 +41,12 @@ func (movieHandler *MovieHandler) writeJSONResponse(w http.ResponseWriter, data 
 }
 
 func (movieHandler *MovieHandler) GetTopMovies(w http.ResponseWriter, r *http.Request) {
-	movies := []models.Movie{
-		{
-			ID:          1,
-			TMDB_ID:     181,
-			Title:       "The Hacker",
-			ReleaseYear: 2022,
-			Genres:      []models.Genre{{ID: 1, Name: "Thriller"}},
-			Keywords:    []string{},
-			Casting:     []models.Actor{{ID: 1, FirstName: "Max", LastName: "Wick"}},
-		},
-		{
-			ID:          2,
-			TMDB_ID:     181,
-			Title:       "Back to the Future",
-			ReleaseYear: 1984,
-			Genres:      []models.Genre{{ID: 1, Name: "Scifi"}},
-			Keywords:    []string{},
-			Casting:     []models.Actor{{ID: 1, FirstName: "Jon", LastName: "Doe"}},
-		},
+	movies, err := movieHandler.storage.GetTopMovies()
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		movieHandler.logger.Error("Get Top Movies Error", err)
+		return
 	}
-
 	movieHandler.writeJSONResponse(w, movies)
 }
 
