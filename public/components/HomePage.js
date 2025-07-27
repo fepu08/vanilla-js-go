8eaae7bf2628a20aa1329f15bc76d36aa2676be8
fepu08 <ferencbesenyei@protonmail.com>
2025-07-27 21:38:48 +0200

    dynamically render component

--- public/components/HomePage.js
+++ public/components/HomePage.js
@@ -1,8 +1,29 @@
+import { API } from '../services/API.js';
+
 export class HomePage extends HTMLElement {
+  async render() {
+    const topMovies = await API.getTopMovies();
+    renderMovies(topMovies, document.querySelector('#top-10 ul'));
+
+    const randomMovies = await API.randomMovies();
+    renderMovies(randomMovies, document.querySelector('#random ul'));
+
+    function renderMovies(movies, ul) {
+      ul.innerHTML = '';
+      movies.forEach((m) => {
+        const li = document.createElement('li');
+        li.innerHTML = m.title;
+        ul.appendChild(li);
+      });
+    }
+  }
+
   connectedCallback() {
     const template = document.getElementById('template-home');
     const content = template.content.cloneNode(true);
     this.appendChild(content);
+
+    this.render();
   }
 }
 
