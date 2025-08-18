ce846b98b01d137f743f1b7360235b200d00852c
fepu08 <ferencbesenyei@protonmail.com>
2025-08-18 14:56:21 +0200

    adding router

---
+++ public/services/Router.js
@@ -0,0 +1,14 @@
+const Router = {
+  init: () => {
+    window.addEventListener('popstate', () => {
+      Router.go(location.pathname, false);
+    });
+
+		Router.go(location.pathname + location.search)
+  },
+  go: (route, addToHistory = true) => {
+    if (addToHistory) {
+      history.pushState(null, '', route);
+    }
+  },
+};
